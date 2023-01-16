a0cfa8318a43c65cc3a82d8e53372ad03fd0147e
asimkhan <asimumair@outlook.com>
2023-01-16 12:33:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ Step 6: Set up Server Js
 Step 7: Test React File Upload API
 Step 8: Build React Single File Upload
 
-import React from 'react';
+import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-function App() {
-  return (
-    <div className="App">
-      <h2>React File Upload Demo</h2>
-    </div>
-  );
+import './App.css';
+import FilesUploadComponent from './components/files-upload-component';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FilesUploadComponent />
+      </div>
+    );
+  }
 }
 export default App;
