c0fc98462ecd19c95ec3823f1c2f22a7907e749f
asimkhan <asimumair@outlook.com>
2023-01-16 12:31:38 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+React Node Multer File Upload Example
+Step 1: Install React App
+Step 2: Create React File Upload Component
+Step 3: Create Node Server
+Step 4: Create Schema
+Step 5: Set up Express Routes
+Step 6: Set up Server Js
+Step 7: Test React File Upload API
+Step 8: Build React Single File Upload
+
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <h2>React File Upload Demo</h2>
+    </div>
+  );
+}
+export default App;
